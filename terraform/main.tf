78c67fd4692d54bf611cab49f2d37fe51d7cd0a1
lidor ettinger <lidor.ettinger@gmail.com>
2023-01-07 01:20:54 +0200

    upload a nodejs app

---
+++ terraform/main.tf
@@ -0,0 +1,172 @@
+# Setup the provider
+provider "aws" {
+  profile = "default"
+  region  = "eu-west-2"
+}
+
+resource "aws_default_vpc" "default_vpc" { }
+
+resource "aws_default_subnet" "default_subnet_a" {
+  availability_zone = "eu-west-2a"
+}
+
+resource "aws_default_subnet" "default_subnet_b" {
+  availability_zone = "eu-west-2b"
+}
+
+resource "aws_default_subnet" "default_subnet_c" {
+  availability_zone = "eu-west-2c"
+}
+
+resource "aws_ecr_repository" "ecr_repo" {
+  name = "node-web-app"
+}
+
+resource "aws_ecs_cluster" "workshop" {
+  name = "workshop" 
+}
+
+
+# Define the task definition
+
+resource "aws_ecs_task_definition" "web_app_task_defintion" {
+  family                   = "web-app" 
+  container_definitions    = <<DEFINITION
+  [
+    {
+      "name": "web-app",
+      "image": "${aws_ecr_repository.ecr_repo.repository_url}",
+      "essential": true,
+      "portMappings": [
+        {
+          "containerPort": 8080,
+          "hostPort": 8080
+        }
+      ],
+      "memory": 512,
+      "cpu": 256
+    }
+  ]
+  DEFINITION
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  memory                   = 512
+  cpu                      = 256
+  execution_role_arn       = "${aws_iam_role.ecsTaskExecutionRole.arn}"
+}
+
+# Setup the IAM Role of the ECS
+####################################################################################
+resource "aws_iam_role" "ecsTaskExecutionRole" {
+  name               = "ecsTaskExecutionRole"
+  assume_role_policy = "${data.aws_iam_policy_document.assume_role_policy.json}"
+}
+
+data "aws_iam_policy_document" "assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
+  role       = "${aws_iam_role.ecsTaskExecutionRole.name}"
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+
+
+# Load Balancer
+####################################################################################
+resource "aws_alb" "load_balancer" {
+  name               = "lb-tf"
+  load_balancer_type = "application"
+  subnets = [
+    "${aws_default_subnet.default_subnet_a.id}",
+    "${aws_default_subnet.default_subnet_b.id}",
+    "${aws_default_subnet.default_subnet_c.id}"
+  ]
+
+  security_groups = ["${aws_security_group.load_balancer_security_group.id}"]
+}
+
+# Creating a security group for the load balancer:
+####################################################################################
+resource "aws_security_group" "load_balancer_security_group" {
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Allowing traffic in from all sources
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Target Group
+####################################################################################
+resource "aws_lb_target_group" "target_group" {
+  name        = "target-group"
+  port        = 80
+  protocol    = "HTTP"
+  target_type = "ip"
+  vpc_id      = "${aws_default_vpc.default_vpc.id}"
+}
+
+resource "aws_lb_listener" "listener" {
+  load_balancer_arn = "${aws_alb.load_balancer.arn}"
+  port              = "80"
+  protocol          = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = "${aws_lb_target_group.target_group.arn}"
+  }
+}
+
+# Setting up an ECS service
+####################################################################################
+resource "aws_ecs_service" "web_app_service" {
+  name            = "my-first-service"
+  cluster         = "${aws_ecs_cluster.workshop.id}"
+  task_definition = "${aws_ecs_task_definition.web_app_task_defintion.arn}"
+  launch_type     = "FARGATE"
+  desired_count   = 1
+
+  load_balancer {
+    target_group_arn = "${aws_lb_target_group.target_group.arn}"
+    container_name   = "${aws_ecs_task_definition.web_app_task_defintion.family}"
+    container_port   = 8080
+  }
+
+  network_configuration {
+    subnets          = ["${aws_default_subnet.default_subnet_a.id}", "${aws_default_subnet.default_subnet_b.id}", "${aws_default_subnet.default_subnet_c.id}"]
+    assign_public_ip = true
+    security_groups  = ["${aws_security_group.service_security_group.id}"]
+  }
+}
+
+
+resource "aws_security_group" "service_security_group" {
+  ingress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    security_groups = ["${aws_security_group.load_balancer_security_group.id}"] # Allowing traffic in from the load balancer security group
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
